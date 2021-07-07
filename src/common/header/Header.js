c3e1457ae418d7ada98b30c84d56e102c3aba574
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-07-07 12:32:34 +0530

    Added PropTypes for TabContainer and added a check for state value when displaying login TabContainer

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
+import PropTypes from 'prop-types';
 
 const customStyles = {
   content :{
@@ -27,6 +28,10 @@ const TabContainer =function(props) {
       {props.children}
     </Typography>
   );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
 class Header extends Component{
@@ -74,6 +79,7 @@ class Header extends Component{
                 <Tab label="Login"/>
                 <Tab label="Register"/>
               </Tabs>
+              {this.state.value == 0  &&
               <TabContainer>
                 <FormControl required>
                   <InputLabel htmlFor="username"> Username </InputLabel>
@@ -84,7 +90,7 @@ class Header extends Component{
                   <Input id="password" type="password" />
                 </FormControl><br/><br/>
                 <Button variant="contained" color="primary"> Login</Button>
-              </TabContainer>
+              </TabContainer>}
           </Modal>
       </div>
     )
